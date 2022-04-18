80e86b07c7939e9a8f987c28a8bfea282c52098f
michael vanderford <michael.vanderford@gmail.com>
2022-04-18 06:20:28 -0500

    moveing files  from electron_file_manager

---
+++ README.md
@@ -0,0 +1,9 @@
+
+EFM - Alpha
+
+File manager for Linux - Built with Electron
+
+
+![Screenshot 1](/screenshots/screenshot1.png?raw=true "Screenshot 1")
+
+![Screenshot 2](/screenshots/screenshot2.png?raw=true "Screenshot 2")
---
+++ prefs.js
@@ -0,0 +1,32 @@
+{
+  "name": "electron_file_manager",
+  "version": "0.0.1",
+  "author": "michael vanderford",
+  "description": "Electron File Manager",
+  "keywords": [
+    "electron",
+    "file",
+    "manager",
+    "js"
+  ],
+  "main": "main.js",
+  "scripts": {
+    "start": "electron ."
+  },
+  "dependencies": {
+    "7zip-bin": "^5.1.1",
+    "bootstrap": "^5.0.1",
+    "chart.js": "^3.5.1",
+    "dragselect": "^2.2.2",
+    "drivelist": "^9.2.4",
+    "jquery": "^3.6.0",
+    "mime-types": "^2.1.35",
+    "mousetrap": "^1.6.5",
+    "node-sqlite": "^0.0.2-security",
+    "usb": "^2.1.2",
+    "webpack": "^5.66.0"
+  },
+  "devDependencies": {
+    "electron": "^15.3.1"
+  }
+}
